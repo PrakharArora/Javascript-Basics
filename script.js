6b29ab010d78bf32e077a9dbae58d7dc2e5f77d8
Prakharr18 <106022380+Prakharr18@users.noreply.github.com>
2022-05-22 20:50:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+let btnCheck = document.querySelector('button');
+let input = document.querySelector('input');
+let result = document.querySelector('h1')
+let maxLength = 20;
+let minLength = 10;
+
+let lowerMinCount = 3;
+const LOWER_REGEX = /([a-z])/g;
+
+let upperMinCount = 2;
+const UPPER_REGEX = /([A-Z])/g;
+
+let numMinCount = 2;
+const NUM_REGEX = /([\d])/g;
+
+let specialMinCount =2;
+const SPECIAL_REGEX = /([!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])/g;
+
+
+
+
+
+btnCheck.addEventListener('click' , ()=>{
+let password = input.value;
+if(password.length > maxLength){
+
+    result.innerText = "Password should be less that 20 chracter"
+    return;
+}
+
+result.innerText = isStrongPassword(password) ? 'STRONG' : 'WEAK'
+
+});
+
+
+function isStrongPassword(password){
+
+    let upperMatch = password.match(UPPER_REGEX) ?? [];
+    let lowerMatch = password.match(LOWER_REGEX) ?? [];
+    let numMatch = password.match(NUM_REGEX) ?? [];
+    let specialrMatch = password.match(SPECIAL_REGEX) ?? [];
+
+
+    return password.length >= minLength &&
+    upperMatch.length >= upperMinCount &&
+    lowerMatch.length >= lowerMinCount &&
+    numMatch.length >= numMinCount &&
+    specialrMatch.length >= specialMinCount
+}
